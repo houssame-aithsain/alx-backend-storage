3342f2b8991c2f9ad6fdc01d0160de9588c9c950
Houssame <hossame@inbox.ru>
2024-10-17 17:16:32 +0000

    1

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,10 @@
+-- Task 1: Create table users with an enumeration for country
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+    PRIMARY KEY (id)
+);
+
