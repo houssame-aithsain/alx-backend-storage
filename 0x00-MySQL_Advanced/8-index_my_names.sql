b4d96f5caf2b5954243b1c1d76de04a52c355b44
Houssame <hossame@inbox.ru>
2024-10-17 17:43:43 +0000

    8

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,11 +1,6 @@
 -- Script to create an index on the first letter of the name column in the names table.
 
 -- Drop the existing index if it exists.
-DROP INDEX IF EXISTS idx_name_first ON names;
-
--- Create a new index on the first letter of the name column.
-CREATE INDEX idx_name_first ON names (SUBSTRING(name, 1, 1));
-
--- Optional: Show the indexes of the names table to confirm the new index has been created.
-SHOW INDEX FROM names;
+CREATE INDEX idx_name_first
+ ON names(name(1));
 
